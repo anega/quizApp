675e71f39806c33274ac245035e20edc0cbcda45
anega <anegamega@gmail.com>
2017-08-14 17:54:53 +0300

    Quiz validation: stage

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.android.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean formIsValid() {
+        EditText nameContents = (EditText) findViewById(R.id.username);
+        String nameStr = nameContents.getText().toString().trim();
+        Boolean validName = !TextUtils.isEmpty(nameStr);
+
+        EditText whaleNameContents = (EditText) findViewById(R.id.whale_name);
+        String whaleNameStr = whaleNameContents.getText().toString().trim();
+        Boolean validWhaleName = whaleNameStr.equals("Willy");
+
+        return (validName && validWhaleName);
+    }
+
+    public void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    public void submitQuiz(View view) {
+        String message = "";
+        if (formIsValid()) {
+            message = "You've passed! Congratulations";
+        } else {
+            message = "Sorry! Think and google.";
+        }
+
+        showToast(message);
     }
 }
